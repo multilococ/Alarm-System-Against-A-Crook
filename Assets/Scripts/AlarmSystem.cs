f385eb420787766e3f5d4743ba7ee425f7e770a0
multilococ <multilococ@yandex.ru>
2025-02-18 01:07:48 +0300

    In progres

---
+++ Assets/Scripts/AlarmSystem.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class AlarmSystem : MonoBehaviour
+{
+
+
+    private void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,44 @@
+using System;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] private Transform[] _wayPoints;
+
+    [SerializeField] private float _speed = 5;
+
+    private Transform _currentWayPoint;
+
+    private int _currentWayPointIndex = 0;
+
+    private float _minDistanceToWayPoint = 0.1f;
+
+    private void Update()
+    {
+        ChooseWayPoint();
+        Move();
+        transform.LookAt(_currentWayPoint);
+    }
+
+    private void Move()
+    {
+        transform.localPosition = Vector3.MoveTowards(transform.localPosition, _currentWayPoint.position, _speed * Time.deltaTime);
+    }
+
+    private void ChooseWayPoint()
+    {
+        if (_wayPoints.Length < 1)
+        {
+            throw new InvalidOperationException();
+        }
+        else
+        {
+            if (transform.position.IsEnoughClose(_wayPoints[_currentWayPointIndex].position, _minDistanceToWayPoint))
+            {
+                _currentWayPointIndex = (_currentWayPointIndex + 1) % _wayPoints.Length;
+            }
+
+            _currentWayPoint = _wayPoints[_currentWayPointIndex];
+        }
+    }
+}
